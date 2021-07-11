2a7a0c8fbaa853e4d2685fb61987266bd8718e18
NaNAGISaSA <cwangzju@126.com>
2021-07-11 10:25:15 +0800

    Monotone Stack With Repeat Numbers

--- src/01.stack_and_queue/08.monotone_stack.cc
+++ src/01.stack_and_queue/08.monotone_stack.cc
@@ -1,4 +1,5 @@
 #include <stack>
+#include <utility>
 
 #include "01.stack_and_queue/08.monotone_stack.h"
 
@@ -31,8 +32,33 @@ std::vector<std::vector<int>> MonotoneStack::get_near_less_no_repeat(const std::
     return result;
 }
 
-std::vector<std::vector<int>> MonotoneStack::get_near_less_with_repeat(const std::vector<int>&) {
-    return {};
+std::vector<std::vector<int>> MonotoneStack::get_near_less_with_repeat(const std::vector<int>& input) {
+    std::stack<std::vector<size_t>> monotone_stack;
+    std::vector<std::vector<int>> result(input.size(), {-1, -1});
+
+    auto reorder_monotone_stack = [&monotone_stack, &result](int index){
+            auto pop_top = std::move(monotone_stack.top());
+            monotone_stack.pop();
+            int left_value = monotone_stack.size() ? static_cast<int>(monotone_stack.top()[monotone_stack.top().size() - 1]) : -1;
+            for(auto& element : pop_top){
+                result[element] = {left_value, index};
+            }
+    };
+
+    for(size_t i = 0; i < input.size(); ++i){
+        while(monotone_stack.size() && input[monotone_stack.top()[0]] > input[i]){
+            reorder_monotone_stack(static_cast<int>(i));
+        }
+        if(monotone_stack.size() && input[monotone_stack.top()[0]] == input[i]){
+            monotone_stack.top().push_back(i);
+        }else{
+            monotone_stack.push({i});
+        }
+    }
+    while(monotone_stack.size()){
+        reorder_monotone_stack(-1);
+    }
+    return result;
 }
 
 }  // namespace coding_interview_guide::stack_and_queue::monotone_stack
--- tests/01.stack_and_queue/08.monotone_stack_test.cc
+++ tests/01.stack_and_queue/08.monotone_stack_test.cc
@@ -5,10 +5,19 @@
 
 using namespace coding_interview_guide::stack_and_queue::monotone_stack;
 
-TEST(MONOTONE_STACK, MONOTONE_STACK_EXAMPLE_TEST) {
+TEST(MONOTONE_STACK, MONOTONE_STACK_NO_REPEAT_EXAMPLE_TEST) {
     const std::vector<int> input{3, 4, 1, 5, 6, 2, 7};
     auto result = MonotoneStack::get_near_less_no_repeat(input);
     const std::vector<std::vector<int>> truth{{-1, 2}, {0, 2}, {-1, -1}, {2, 5}, {3, 5}, {2, -1}, {5, -1}};
+    for (size_t i = 0; i < input.size(); ++i) {
+        ASSERT_THAT(result[i], truth[i]);
+    }
+}
+
+TEST(MONOTONE_STACK, MONOTONE_STACK_WITH_REPEAT_EXAMPLE_TEST) {
+    const std::vector<int> input{3, 1, 3, 4, 3, 5, 3, 2, 2};
+    auto result = MonotoneStack::get_near_less_with_repeat(input);
+    const std::vector<std::vector<int>> truth{{-1, 1}, {-1, -1}, {1, 7}, {2, 4}, {1, 7}, {4, 6}, {1, 7}, {1, -1}, {1, -1}};
     for (size_t i = 0; i < input.size(); ++i) {
         ASSERT_THAT(result[i], truth[i]);
     }
