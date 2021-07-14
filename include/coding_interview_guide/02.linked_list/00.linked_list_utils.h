6dd27d3c17d54d4fc533b63a37ce8e78d4466e44
NaNAGISaSA <cwangzju@126.com>
2021-07-14 21:53:11 +0800

    Finish RAII

--- include/coding_interview_guide/02.linked_list/00.linked_list_utils.h
+++ include/coding_interview_guide/02.linked_list/00.linked_list_utils.h
@@ -1,9 +1,7 @@
 #ifndef _CODEING_INTERVIEW_GUIDE_LINKED_LIST_LINKED_LIST_UTILS_H_
 #define _CODEING_INTERVIEW_GUIDE_LINKED_LIST_LINKED_LIST_UTILS_H_
 
-#include <vector>
-
-namespace coding_interview_guide::linked_list {
+namespace coding_interview_guide::linked_list::utils {
 
 template <typename T>
 struct Node {
@@ -11,48 +9,6 @@ struct Node {
     }
     T val;
     Node* next;
-};
-
-template <typename T>
-struct LinkedListRAII {
-
-    LinkedListRAII() = default;
-
-    ~LinkedListRAII() {
-        auto deconstruct_linked_list = [](Node<T>* head) {
-            Node<T>* current_head = head;
-            Node<T>* delete_head = current_head;
-            while(delete_head != nullptr){
-                current_head = current_head->next;
-                delete delete_head;
-                delete_head = current_head;
-            }
-        };
-
-        for(auto it = head_vector.cbegin(); it != head_vector.cend(); ++it){
-            deconstruct_linked_list(*it);
-        }
-    }
-
-    template <typename... Ts>
-    Node<T>* make_linked_list(T&& head, Ts&&... values){
-        head_vector.push_back(construct_linked_list(std::move(head), std::forward<Ts>(values)...));
-        return head_vector.back();
-    }
-
-private:
-    Node<T>* construct_linked_list(T&& head) {
-        return new Node(std::forward<T>(head));
-    }
-
-    template <typename... Ts>
-    Node<T>* construct_linked_list(T&& head, Ts&&... values){
-        Node<T>* node = new Node(std::forward<T>(head));
-        node->next = construct_linked_list(std::forward<Ts>(values)...);
-        return node;
-    }
-
-    std::vector<Node<T>*> head_vector;
 };
 
 }
--- tests/02.linked_list/01.ordered_linked_lists_public_parts_test.cc
+++ tests/02.linked_list/01.ordered_linked_lists_public_parts_test.cc
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include "linked_list_test_utils.h"
 #include "02.linked_list/01.ordered_linked_lists_public_parts.h"
 
-using namespace coding_interview_guide::linked_list;
+using namespace coding_interview_guide::linked_list::utils;
 using namespace coding_interview_guide::linked_list::ordered_linked_list_public_part;
 
 TEST(ORDERED_LINKED_LIST_PUBLIC_PART, ORDERED_LINKED_LIST_PUBLIC_PART_EXAMPLE_TEST){
@@ -12,4 +13,8 @@ TEST(ORDERED_LINKED_LIST_PUBLIC_PART, ORDERED_LINKED_LIST_PUBLIC_PART_EXAMPLE_TE
     Node<int>* head2 = wrapper.make_linked_list(1, 2, 3, 8, 9, 10, 15);
     std::vector<int> result = LinkedListPublicPart::find_public_part(head1, head2);
     ASSERT_THAT(result, testing::ElementsAre(1, 2, 8, 10));
+    Node<int>* head3 = wrapper.make_linked_list(2);
+    Node<int>* head4 = wrapper.make_linked_list(1, 2);
+    std::vector<int> result2 = LinkedListPublicPart::find_public_part(head3, head4);
+    ASSERT_THAT(result2, testing::ElementsAre(2));
 }
