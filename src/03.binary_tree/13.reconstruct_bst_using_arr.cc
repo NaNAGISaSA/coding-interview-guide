e2e519bb835b8a8d2298b290a1455642c920f521
NaNAGISaSA <cwangzju@126.com>
2021-08-12 18:41:15 +0800

    Finish Reconstruct Binary Tree Using Arr

--- src/03.binary_tree/13.reconstruct_bst_using_arr.cc
+++ src/03.binary_tree/13.reconstruct_bst_using_arr.cc
@@ -32,7 +32,37 @@ bool ReconstructBstUsingArr::can_reconstruct(const std::vector<int>& array) {
     return array.size() ? can_reconstruct_internal(array, 0, array.size() - 1) : false;
 }
 
-// const Node<int>* ReconstructBstUsingArr::reconstruct(const std::vector<int>& array) {
-// }
+namespace {
+Node<int>* reconstruct_internal(const std::vector<int>& array, size_t start, size_t end) {
+    if (start == end) {
+        return new Node<int>(static_cast<int>(array[end]));
+    }
+    long less = -1;
+    size_t more = end;
+    for (size_t i = start; i < end; ++i) {
+        if (array[i] < array[end]) {
+            less = i;
+        } else {
+            if (more == end) {
+                more = i;
+            }
+        }
+    }
+    Node<int>* node = new Node<int>(static_cast<int>(array[end]));
+    if (less == -1) {
+        node->right = reconstruct_internal(array, start, end - 1);
+    } else if (more == end) {
+        node->left = reconstruct_internal(array, start, end - 1);
+    } else {
+        node->left = reconstruct_internal(array, 0, less);
+        node->right = reconstruct_internal(array, more, end - 1);
+    }
+    return node;
+}
+}  // namespace
+
+const Node<int>* ReconstructBstUsingArr::reconstruct(const std::vector<int>& array) {
+    return array.size() ? reconstruct_internal(array, 0, array.size() - 1) : nullptr;
+}
 
 }  // namespace coding_interview_guide::binary_tree::reconstruct_bst_using_arr
--- tests/03.binary_tree/13.reconstruct_bst_using_arr_test.cc
+++ tests/03.binary_tree/13.reconstruct_bst_using_arr_test.cc
@@ -5,9 +5,19 @@
 using namespace coding_interview_guide::binary_tree;
 using namespace coding_interview_guide::binary_tree::reconstruct_bst_using_arr;
 
-TEST(RECONSTRUCT_BST_USING_ARR, RECONSTRUCT_BST_USING_ARR_EXAMPLE_TEST) {
+TEST(RECONSTRUCT_BST_USING_ARR, RECONSTRUCT_BST_USING_ARR_EXAMPLE_TEST1) {
     std::vector<int> vec1{2, 3, 1};
     ASSERT_TRUE(ReconstructBstUsingArr::can_reconstruct(vec1));
     std::vector<int> vec2{3, 1, 2};
     ASSERT_FALSE(ReconstructBstUsingArr::can_reconstruct(vec2));
+}
+
+TEST(RECONSTRUCT_BST_USING_ARR, RECONSTRUCT_BST_USING_ARR_EXAMPLE_TEST2) {
+    std::vector<int> vec1{2, 3, 1};
+    auto root = ReconstructBstUsingArr::reconstruct(vec1);
+    ASSERT_EQ(root->val, 1);
+    ASSERT_EQ(root->left, nullptr);
+    ASSERT_EQ(root->right->val, 3);
+    ASSERT_EQ(root->right->left->val, 2);
+    ASSERT_EQ(root->right->right, nullptr);
 }
