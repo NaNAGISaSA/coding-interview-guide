f2631b12d33c89e7bc3b5aaaeda1be25a863c808
NaNAGISaSA <cwangzju@126.com>
2021-08-26 21:09:29 +0800

    Balloon Game Dp Method

--- src/04.dynamic_programming/06.balloon_game_max_point.cc
+++ src/04.dynamic_programming/06.balloon_game_max_point.cc
@@ -24,12 +24,40 @@ unsigned long BalloonGameMaxPoint::max_point(const std::vector<unsigned int>& ve
         return 0;
     } else if (vec.size() == 1) {
         return vec[0];
-    } else {
-        std::vector<unsigned int> vec_copy = vec;
-        vec_copy.insert(vec_copy.begin(), 1);
-        vec_copy.push_back(1);
-        return max_point_internal(vec_copy, 1, vec.size());
     }
+    std::vector<unsigned int> vec_copy = vec;
+    vec_copy.insert(vec_copy.begin(), 1);
+    vec_copy.push_back(1);
+    return max_point_internal(vec_copy, 1, vec.size());
+}
+
+unsigned long BalloonGameMaxPoint::max_point_dp(const std::vector<unsigned int>& vec) {
+    size_t balloon_number = vec.size();
+    if (balloon_number == 0) {
+        return 0;
+    } else if (balloon_number == 1) {
+        return vec[0];
+    }
+    std::vector<unsigned int> vec_copy = vec;
+    vec_copy.insert(vec_copy.begin(), 1);
+    vec_copy.push_back(1);
+    std::vector<std::vector<long>> dp_matrix(balloon_number + 2, std::vector<long>(balloon_number + 2, 0L));
+    for (size_t i = 1; i <= balloon_number; ++i) {
+        dp_matrix[i][i] = vec_copy[i - 1] * vec_copy[i] * vec_copy[i + 1];
+    }
+    for (size_t start = balloon_number - 1; start >= 1; --start) {
+        for (size_t end = start + 1; end <= balloon_number; ++end) {
+            dp_matrix[start][end] =
+                std::max(vec_copy[start - 1] * vec_copy[start] * vec_copy[end + 1] + dp_matrix[start + 1][end],
+                         vec_copy[start - 1] * vec_copy[end] * vec_copy[end + 1] + dp_matrix[start][end - 1]);
+            for (size_t i = start + 1; i < end; ++i) {
+                dp_matrix[start][end] = std::max(dp_matrix[start][end],
+                                                 vec_copy[start - 1] * vec_copy[i] * vec_copy[end + 1] +
+                                                     dp_matrix[start][i - 1] + dp_matrix[i + 1][end]);
+            }
+        }
+    }
+    return dp_matrix[1][balloon_number];
 }
 
 }  // namespace coding_interview_guide::dynamic_programming::balloon_game_max_point
--- tests/04.dynamic_programming/06.balloon_game_max_point_test.cc
+++ tests/04.dynamic_programming/06.balloon_game_max_point_test.cc
@@ -9,3 +9,9 @@ TEST(BALLOON_GAME_MAX_POINT, BALLOON_GAME_MAX_POINT_EXAMPLE_TEST) {
     ASSERT_EQ(BalloonGameMaxPoint::max_point(vec1), 50);
     ASSERT_EQ(BalloonGameMaxPoint::max_point(vec2), 639019);
 }
+
+TEST(BALLOON_GAME_MAX_POINT, BALLOON_GAME_MAX_POINT_DP_EXAMPLE_TEST) {
+    std::vector<unsigned int> vec1{3, 2, 5}, vec2{23, 4, 45, 65, 23, 43, 54, 56};
+    ASSERT_EQ(BalloonGameMaxPoint::max_point_dp(vec1), 50);
+    ASSERT_EQ(BalloonGameMaxPoint::max_point_dp(vec2), 639019);
+}
