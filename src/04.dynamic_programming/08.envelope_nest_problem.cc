811fe5ba46d9b03591e8e0d8570b094c571ee826
NaNAGISaSA <cwangzju@126.com>
2021-08-29 15:50:51 +0800

    Nested Envelope O(NlogN) Method

--- src/04.dynamic_programming/08.envelope_nest_problem.cc
+++ src/04.dynamic_programming/08.envelope_nest_problem.cc
@@ -4,35 +4,54 @@
 
 namespace coding_interview_guide::dynamic_programming::envelope_nest_problem {
 
-namespace {
-void max_increase_sub_arr(std::vector<unsigned int>& dp_vec, const std::vector<std::vector<unsigned int>>& envelopes) {
-    for (size_t i = 1; i < envelopes.size(); ++i) {
-        for (size_t j = 0; j < i; ++j) {
-            if (envelopes[j][1] < envelopes[i][1] && dp_vec[j] >= dp_vec[i]) {
-                dp_vec[i] = dp_vec[j] + 1;
-            }
-        }
-    }
-}
-}  // namespace
-
 unsigned int EnvelopeNestProblem::nest_number(const std::vector<std::vector<unsigned int>>& envelopes) {
-    std::vector<unsigned int> dp_vec_width(envelopes.size(), 1);
+    std::vector<unsigned int> dp_vec(envelopes.size(), 1);
     auto envelopes_copy = envelopes;
     std::sort(envelopes_copy.begin(), envelopes_copy.end(), [](const auto& a, const auto& b) {
         return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
     });
-    max_increase_sub_arr(dp_vec_width, envelopes_copy);
-    unsigned int max_number = 0;
-    for (size_t i = 0; i < envelopes_copy.size(); ++i) {
-        if (dp_vec_width[i] > max_number) {
-            max_number = dp_vec_width[i];
+    unsigned int max_nest = 0;
+    for (size_t i = 1; i < envelopes_copy.size(); ++i) {
+        for (size_t j = 0; j < i; ++j) {
+            if (envelopes_copy[j][1] < envelopes_copy[i][1] && dp_vec[j] >= dp_vec[i]) {
+                dp_vec[i] = dp_vec[j] + 1;
+            }
+        }
+        if (dp_vec[i] > max_nest) {
+            max_nest = dp_vec[i];
         }
     }
-    return max_number;
+    return max_nest;
 }
 
-// unsigned int EnvelopeNestProblem::nest_number_time_opt(const std::vector<std::vector<unsigned int>>& envelopes) {
-// }
+unsigned int EnvelopeNestProblem::nest_number_time_opt(const std::vector<std::vector<unsigned int>>& envelopes) {
+    auto envelopes_copy = envelopes;
+    std::sort(envelopes_copy.begin(), envelopes_copy.end(), [](const auto& a, const auto& b) {
+        return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+    });
+    unsigned int max_nest = 0;
+    std::vector<unsigned int> minimun_ends(envelopes_copy.size(), 0);
+    minimun_ends[0] = envelopes_copy[0][1];
+    size_t ends_right = 0;
+    for (size_t i = 1; i < envelopes_copy.size(); ++i) {
+        size_t left = 0;
+        size_t right = ends_right;
+        while (left <= right) {
+            size_t middle = (left + right) / 2;
+            if (envelopes_copy[i][1] > minimun_ends[middle]) {
+                left = middle + 1;
+            } else {
+                if (middle == 0) {
+                    break;
+                }
+                right = middle - 1;
+            }
+        }
+        minimun_ends[left] = envelopes_copy[i][1];
+        ends_right = std::max(ends_right, left);
+        max_nest = std::max(max_nest, static_cast<unsigned int>(left + 1));
+    }
+    return max_nest;
+}
 
 }  // namespace coding_interview_guide::dynamic_programming::envelope_nest_problem
--- tests/04.dynamic_programming/08.envelope_nest_problem_test.cc
+++ tests/04.dynamic_programming/08.envelope_nest_problem_test.cc
@@ -11,10 +11,9 @@ TEST(ENVELOPE_NEST_PROBLEM, ENVELOPE_NEST_PROBLEM_EXAMPLE_TEST) {
     ASSERT_EQ(EnvelopeNestProblem::nest_number(vec2), 1);
 }
 
-// TEST(ENVELOPE_NEST_PROBLEM, ENVELOPE_NEST_PROBLEM_OPT_EXAMPLE_TEST) {
-//     std::vector<std::vector<unsigned int>> vec1{{3, 4}, {2, 3}, {4, 5}, {1, 3}, {2, 2}, {3, 6}, {1, 2}, {3, 2}, {2,
-//     4}},
-//         vec2{{1, 4}, {4, 1}};
-//     ASSERT_EQ(EnvelopeNestProblem::nest_number_time_opt(vec1), 4);
-//     ASSERT_EQ(EnvelopeNestProblem::nest_number_time_opt(vec2), 1);
-// }
+TEST(ENVELOPE_NEST_PROBLEM, ENVELOPE_NEST_PROBLEM_OPT_EXAMPLE_TEST) {
+    std::vector<std::vector<unsigned int>> vec1{{3, 4}, {2, 3}, {4, 5}, {1, 3}, {2, 2}, {3, 6}, {1, 2}, {3, 2}, {2, 4}},
+        vec2{{1, 4}, {4, 1}};
+    ASSERT_EQ(EnvelopeNestProblem::nest_number_time_opt(vec1), 4);
+    ASSERT_EQ(EnvelopeNestProblem::nest_number_time_opt(vec2), 1);
+}
