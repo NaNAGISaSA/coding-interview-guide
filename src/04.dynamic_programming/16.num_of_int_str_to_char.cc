542fcda112d8d46fd06b20db09b60a5ae1af08b1
NaNAGISaSA <cwangzju@126.com>
2021-09-06 19:03:03 +0800

    Opt Integer To Str

--- src/04.dynamic_programming/16.num_of_int_str_to_char.cc
+++ src/04.dynamic_programming/16.num_of_int_str_to_char.cc
@@ -3,35 +3,21 @@
 namespace coding_interview_guide::dynamic_programming::num_of_int_str_to_char {
 
 unsigned long IntStrToChar::number(const std::string& str) {
-    if (str[0] == '0') {
-        return 0L;
+    if (str.size() == 0) {
+        return 0;
     }
-    if (str.size() <= 1) {
-        return str.size();
-    }
-
-    unsigned long pre_one = 1UL, swap = 0UL, current = 1UL;
-    if (str[1] == '0') {
-        if (str[0] != '1' && str[0] != '2') {
-            return 0L;
-        }
-    } else if (str[0] == '1' || (str[0] == '2' && !(str[1] == '7' || str[1] == '8' || str[1] == '9'))) {
-        current = 2L;
-    }
-
-    for (size_t i = 2; i < str.size(); ++i) {
+    unsigned long pre = 1UL, swap = 0UL;
+    unsigned long current = str[0] == '0' ? 0UL : 1UL;
+    for (size_t i = 1; i < str.size(); ++i) {
         swap = current;
         if (str[i] == '0') {
-            if (str[i - 1] != '1' && str[i - 1] != '2') {
-                return 0L;
-            }
-            current = pre_one;
+            current = (str[i - 1] == '1' || str[i - 1] == '2') ? pre : 0UL;
         } else {
             if (str[i - 1] == '1' || (str[i - 1] == '2' && !(str[i] == '7' || str[i] == '8' || str[i] == '9'))) {
-                current += pre_one;
+                current += pre;
             }
         }
-        pre_one = swap;
+        pre = swap;
     }
     return current;
 }
--- tests/04.dynamic_programming/16.num_of_int_str_to_char_test.cc
+++ tests/04.dynamic_programming/16.num_of_int_str_to_char_test.cc
@@ -5,9 +5,9 @@
 using namespace coding_interview_guide::dynamic_programming::num_of_int_str_to_char;
 
 TEST(NUM_OF_INT_STR_TO_CHAR, NUM_OF_INT_STR_TO_CHAR_EXAMPLE_TEST) {
-    std::string str1("1111"), str2("01"), str3("10"), str4("11311");
+    std::string str1("1111"), str2("10"), str3("110011"), str4("11311");
     ASSERT_EQ(IntStrToChar::number(str1), 5);
-    ASSERT_EQ(IntStrToChar::number(str2), 0);
-    ASSERT_EQ(IntStrToChar::number(str3), 1);
+    ASSERT_EQ(IntStrToChar::number(str2), 1);
+    ASSERT_EQ(IntStrToChar::number(str3), 0);
     ASSERT_EQ(IntStrToChar::number(str4), 6);
 }
