3e5a78bf8e0cff1103bdd1267291cd05e831cc98
NaNAGISaSA <cwangzju@126.com>
2021-10-04 11:42:11 +0800

    Finish Div Bit Operation

--- src/07.bitwise/03.arithmetic_operations.cc
+++ src/07.bitwise/03.arithmetic_operations.cc
@@ -1,3 +1,5 @@
+#include <limits>
+
 #include "07.bitwise/03.arithmetic_operations.h"
 
 /*
@@ -20,6 +22,14 @@ int ArithmeticOperations::sub(int a, int b) {
 }
 
 int ArithmeticOperations::multi(int a, int b) {
+    if (a == std::numeric_limits<int>::min() || b == std::numeric_limits<int>::min()) {
+        if (a == 0 || b == 0) {
+            return 0;
+        } else if (a == 1 || b == 1) {
+            return std::numeric_limits<int>::min();
+        }
+    }
+
     int result = 0;
     int sign_a = (a >> 31) & 1, sign_b = (b >> 31) & 1;
     if (sign_a) {
@@ -38,6 +48,35 @@ int ArithmeticOperations::multi(int a, int b) {
     return sign_a == sign_b ? result : add(~result, 1);
 }
 
-// int ArithmeticOperations::div(int a, int b);
+int ArithmeticOperations::div(int a, int b) {
+    auto divide = [](int a, int b) {
+        int result = 0;
+        int sign_a = (a >> 31) & 1, sign_b = (b >> 31) & 1;
+        if (sign_a) {
+            a = add(~a, 1);
+        }
+        if (sign_b) {
+            b = add(~b, 1);
+        }
+        for (int i = 31; i >= 0; i = sub(i, 1)) {
+            if ((a >> i) >= b) {
+                result |= (1 << i);
+                a = sub(a, b << i);
+            }
+        }
+        return sign_a == sign_b ? result : add(~result, 1);
+    };
+
+    if (a == std::numeric_limits<int>::min() && b == std::numeric_limits<int>::min()) {
+        return 1;
+    } else if (a == std::numeric_limits<int>::min()) {
+        int internal = divide(add(a, 1), b);
+        return add(internal, divide(sub(a, multi(internal, b)), b));
+    } else if (b == std::numeric_limits<int>::min()) {
+        return 0;
+    } else {
+        return divide(a, b);
+    }
+}
 
 }  // namespace coding_interview_guide::bitwise::arithmetic_operations
--- tests/07.bitwise/03.arithmetic_operations_test.cc
+++ tests/07.bitwise/03.arithmetic_operations_test.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <limits>
 
 #include "07.bitwise/03.arithmetic_operations.h"
 
@@ -27,4 +28,12 @@ TEST(ARITHMETIC_OPERATIONS, ARITHMETIC_OPERATIONS_MULTI_EXAMPLE_TEST) {
     ASSERT_EQ(ArithmeticOperations::multi(-3, -4), 12);
     ASSERT_EQ(ArithmeticOperations::multi(-3, 0), 0);
     ASSERT_EQ(ArithmeticOperations::multi(0, -3), 0);
+}
+
+TEST(ARITHMETIC_OPERATIONS, ARITHMETIC_OPERATIONS_DIV_EXAMPLE_TEST) {
+    ASSERT_EQ(ArithmeticOperations::div(3, 4), 0);
+    ASSERT_EQ(ArithmeticOperations::div(-8, -3), 2);
+    ASSERT_EQ(ArithmeticOperations::div(-9, 3), -3);
+    ASSERT_EQ(ArithmeticOperations::div(12, -3), -4);
+    ASSERT_EQ(ArithmeticOperations::div(std::numeric_limits<int>::min(), -4), 536870912);
 }
