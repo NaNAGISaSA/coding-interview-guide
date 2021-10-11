4504a04f1097f4d34a198c809b882b1e4c201da8
NaNAGISaSA <cwangzju@126.com>
2021-10-11 17:30:38 +0800

    Finish Max Subarr Len EQ K Series

--- src/08.vector_and_matrix/11.max_subarr_len_eq_k_series.cc
+++ src/08.vector_and_matrix/11.max_subarr_len_eq_k_series.cc
@@ -24,4 +24,40 @@ unsigned int MaxSubarrLenEqKSeries::problem1(const std::vector<int>& vec, int k)
     return max_length;
 }
 
+unsigned int MaxSubarrLenEqKSeries::problem2(const std::vector<int>& vec) {
+    if (vec.size() == 0) {
+        return 0U;
+    }
+    std::unordered_map<int, size_t> map{{0, -1}};
+    int sum = 0;
+    unsigned int max_length = 0U;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        sum += vec[i] > 0 ? 1 : (vec[i] == 0 ? 0 : -1);
+        if (map.find(sum) != map.end()) {
+            max_length = std::max(max_length, static_cast<unsigned int>(i - map[sum]));
+        } else {
+            map.insert({sum, i});
+        }
+    }
+    return max_length;
+}
+
+unsigned int MaxSubarrLenEqKSeries::problem3(const std::vector<unsigned int>& vec) {
+    if (vec.size() == 0) {
+        return 0U;
+    }
+    std::unordered_map<int, size_t> map{{0, -1}};
+    int sum = 0;
+    unsigned int max_length = 0U;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        sum += vec[i] == 1 ? 1 : -1;
+        if (map.find(sum) != map.end()) {
+            max_length = std::max(max_length, static_cast<unsigned int>(i - map[sum]));
+        } else {
+            map.insert({sum, i});
+        }
+    }
+    return max_length;
+}
+
 }  // namespace coding_interview_guide::vector_and_matrix::max_subarr_len_eq_k_series
--- tests/08.vector_and_matrix/11.max_subarr_len_eq_k_series_test.cc
+++ tests/08.vector_and_matrix/11.max_subarr_len_eq_k_series_test.cc
@@ -8,3 +8,16 @@ TEST(MAX_SUBARR_LEN_EQ_K_SERIES, MAX_SUBARR_LEN_EQ_K_SERIES_P1_TEST) {
     ASSERT_EQ(MaxSubarrLenEqKSeries::problem1({1, -2, 1, 1, 1}, 0), 3);
     ASSERT_EQ(MaxSubarrLenEqKSeries::problem1({1, -2, 1, 1, 1}, 1), 4);
 }
+
+TEST(MAX_SUBARR_LEN_EQ_K_SERIES, MAX_SUBARR_LEN_EQ_K_SERIES_P2_TEST) {
+    ASSERT_EQ(MaxSubarrLenEqKSeries::problem2({1, -2}), 2);
+    ASSERT_EQ(MaxSubarrLenEqKSeries::problem2({-1, 0}), 1);
+    ASSERT_EQ(MaxSubarrLenEqKSeries::problem2({1, -2, 0, -1, 1, 1}), 5);
+    ASSERT_EQ(MaxSubarrLenEqKSeries::problem2({1, 1, 0, -1, 0}), 4);
+}
+
+TEST(MAX_SUBARR_LEN_EQ_K_SERIES, MAX_SUBARR_LEN_EQ_K_SERIES_P3_TEST) {
+    ASSERT_EQ(MaxSubarrLenEqKSeries::problem3({1, 0}), 2);
+    ASSERT_EQ(MaxSubarrLenEqKSeries::problem3({1, 1}), 0);
+    ASSERT_EQ(MaxSubarrLenEqKSeries::problem3({1, 1, 0, 0, 1, 1}), 4);
+}
