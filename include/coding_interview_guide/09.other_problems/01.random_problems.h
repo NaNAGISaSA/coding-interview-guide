14e7ee2a99bb3ec9dcd816c5848ba2b9404c4574
NaNAGISaSA <cwangzju@126.com>
2021-11-01 16:59:00 +0800

    Finish Reservoir Algorithm

--- include/coding_interview_guide/09.other_problems/01.random_problems.h
+++ include/coding_interview_guide/09.other_problems/01.random_problems.h
@@ -8,7 +8,14 @@
 【补充】
 给定一个以p概率产生0，以1-p概率产生1的随机函数random01p，除此之外，不能使用任何随机数机制。
 请使用该函数设计一个等概率随机产生1~6的随机函数random_one_to_six。
+
+【题目】
+有一台机器按照自然数序列的方式吐球（1号球，2号球，3号球），有一个袋子，袋子最多只能够装下K个球，
+除了这个袋子之外，没有更多的储存空间。设计一种选择方式，使得当机器吐出第N号球时（N>K），袋子中的
+球个数是K个，同时保证从开始至当前所有球每一个被选入袋子的概率均为K/N。
 */
+
+#include <vector>
 
 namespace coding_interview_guide::other_problems::random_problems {
 
@@ -16,6 +23,11 @@ class RandomProblems {
 public:
     static int random_one_to_seven();
     static int random_one_to_six();
+};
+
+class ReservoirAlgorithm {
+public:
+    std::vector<size_t> select(size_t k, size_t n);
 };
 
 }  // namespace coding_interview_guide::other_problems::random_problems
--- src/09.other_problems/01.random_problems.cc
+++ src/09.other_problems/01.random_problems.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <random>
 
 #include "09.other_problems/01.random_problems.h"
@@ -17,6 +18,10 @@ int random_one_to_five() {
 
 int random01p() {
     return random_internal(0, 1);
+}
+
+size_t random1n(size_t n) {
+    return static_cast<size_t>(random_internal(1, static_cast<int>(n)));
 }
 }  // namespace
 
@@ -42,6 +47,31 @@ int RandomProblems::random_one_to_six() {
         num = equal03() + equal03() * 4;
     } while (num > 11);
     return 1 + num % 6;
+}
+
+/*
+蓄水池算法：
+1. 处理1~k号球时，直接放进袋子里；
+2. 处理第i号球时（i>k）时，以k/i的概率决定是否将当前球放入袋子中；如果决定放入，则随机
+将当前袋中的一个球取出丢弃。
+
+使用如上算法，假设当前放置到第x号球，其被放入的概率为k/x；
+对于已经被放入袋子中的球y，其在x之后依旧在袋子中的概率为：k/y * y/y+1 * ... * x-1/x = k/x.
+*/
+std::vector<size_t> ReservoirAlgorithm::select(size_t k, size_t n) {
+    if (k == 0 || n == 0) {
+        return {};
+    }
+    std::vector<size_t> result(std::min(k, n), 0);
+    for (size_t i = 1; i <= result.size(); ++i) {
+        result[i] = i;
+    }
+    for (size_t i = k + 1; i <= n; ++i) {
+        if (random1n(i) <= k) {
+            result[random1n(k) - 1] = i;
+        }
+    }
+    return result;
 }
 
 }  // namespace coding_interview_guide::other_problems::random_problems
