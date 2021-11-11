968f7da4ba666b60cc2137f1f1842dd22212ab76
NaNAGISaSA <cwangzju@126.com>
2021-11-11 19:20:35 +0800

    Finish One Appear Count Opt Method

--- src/09.other_problems/18.one_appear_count.cc
+++ src/09.other_problems/18.one_appear_count.cc
@@ -1,3 +1,6 @@
+#include <cmath>
+#include <iostream>
+
 #include "09.other_problems/18.one_appear_count.h"
 
 namespace coding_interview_guide::other_problems::one_appear_count {
@@ -20,7 +23,27 @@ unsigned int OneAppearCount::count(unsigned int num) {
     return result;
 }
 
-// unsigned int OneAppearCount::count_opt(unsigned int num) {
-// }
+unsigned int OneAppearCount::count_opt(unsigned int num) {
+    if (num == 0) {
+        return 0U;
+    }
+    auto get_digit_num = [](int number) {
+        unsigned int digit_num = 0U;
+        while (number != 0) {
+            number /= 10;
+            ++digit_num;
+        }
+        return digit_num;
+    };
+    unsigned int digit_num = get_digit_num(num);
+    if (digit_num == 1) {
+        return 1U;
+    }
+    unsigned int pow_num = static_cast<unsigned int>(std::pow(10, --digit_num));
+    unsigned int head_num = num / pow_num;
+    unsigned int head = head_num == 1 ? num % pow_num + 1 : pow_num;
+    unsigned int other = head_num * digit_num * (pow_num / 10);
+    return head + other + count_opt(num % pow_num);
+}
 
 }  // namespace coding_interview_guide::other_problems::one_appear_count
--- tests/09.other_problems/18.one_appear_count_test.cc
+++ tests/09.other_problems/18.one_appear_count_test.cc
@@ -7,11 +7,13 @@ using namespace coding_interview_guide::other_problems::one_appear_count;
 TEST(ONE_APPEAR_COUNT, ONE_APPEAR_COUNT_EXAMPLE_TEST) {
     ASSERT_EQ(OneAppearCount::count(5), 1);
     ASSERT_EQ(OneAppearCount::count(11), 4);
+    ASSERT_EQ(OneAppearCount::count(300), 160);
     ASSERT_EQ(OneAppearCount::count(2345), 1775);
 }
 
-// TEST(ONE_APPEAR_COUNT, ONE_APPEAR_COUNT_OPT_EXAMPLE_TEST) {
-//     ASSERT_EQ(OneAppearCount::count_opt(5), 1);
-//     ASSERT_EQ(OneAppearCount::count_opt(11), 4);
-//     ASSERT_EQ(OneAppearCount::count_opt(2345), 1775);
-// }
+TEST(ONE_APPEAR_COUNT, ONE_APPEAR_COUNT_OPT_EXAMPLE_TEST) {
+    ASSERT_EQ(OneAppearCount::count_opt(5), 1);
+    ASSERT_EQ(OneAppearCount::count_opt(11), 4);
+    ASSERT_EQ(OneAppearCount::count_opt(300), 160);
+    ASSERT_EQ(OneAppearCount::count_opt(2345), 1775);
+}
