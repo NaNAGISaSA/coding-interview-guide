f65fc438518d8f5a26313c1942528c3576343457
NaNAGISaSA <cwangzju@126.com>
2021-11-17 21:02:34 +0800

    Finish Divide Candy Problem Adv

--- src/09.other_problems/23.divide_candy_problem.cc
+++ src/09.other_problems/23.divide_candy_problem.cc
@@ -26,6 +26,7 @@ unsigned int DivideCandyProblem::min_candy_num1(const std::vector<unsigned int>&
 
     unsigned int index = next_left_slope(0);
     add_right_slope(0, index++);
+
     unsigned int lbase = 1, next_left_start = 0;
     while (index < vec_size) {
         if (point[index] == point[index - 1]) {
@@ -46,7 +47,66 @@ unsigned int DivideCandyProblem::min_candy_num1(const std::vector<unsigned int>&
     return result;
 }
 
-// unsigned int DivideCandyProblem::min_candy_num2(const std::vector<unsigned int>& point) {
-// }
+unsigned int DivideCandyProblem::min_candy_num2(const std::vector<unsigned int>& point) {
+    unsigned int vec_size = static_cast<unsigned int>(point.size());
+    if (vec_size == 0) {
+        return 0U;
+    }
+
+    unsigned int result = 0;
+
+    auto next_left_slope = [&point, &vec_size](unsigned int start) {
+        for (unsigned int i = start; i < vec_size - 1; ++i) {
+            if (point[i] < point[i + 1]) {
+                return i;
+            }
+        }
+        return vec_size - 1;
+    };
+
+    auto add_right_slope = [&point, &result](long start, long end) {
+        unsigned int base = 1;
+        result += base;
+        for (long i = end - 1; i >= start; --i) {
+            if (point[i] == point[i + 1]) {
+                result += base;
+            } else {
+                result += ++base;
+            }
+        }
+        return base;
+    };
+
+    unsigned int index = next_left_slope(0);
+    add_right_slope(0, index++);
+
+    unsigned int lbase = 1, same = 1;
+    unsigned int next_left_start = 0, right_slope_len = 0;
+    while (index < vec_size) {
+        if (point[index] == point[index - 1]) {
+            result += lbase;
+            ++same;
+            ++index;
+        } else if (point[index] > point[index - 1]) {
+            result += ++lbase;
+            same = 1;
+            ++index;
+        } else {
+            next_left_start = next_left_slope(index);
+            right_slope_len = add_right_slope(index - 1, next_left_start);
+            if (lbase >= right_slope_len) {
+                result -= right_slope_len;
+            } else if (same == 1) {
+                result -= lbase;
+            } else {
+                result = result + (right_slope_len - lbase) * same - right_slope_len;
+            }
+            index = next_left_start + 1;
+            same = 1;
+            lbase = 1;
+        }
+    }
+    return result;
+}
 
 }  // namespace coding_interview_guide::other_problems::divide_candy_problem
--- tests/09.other_problems/23.divide_candy_problem_test.cc
+++ tests/09.other_problems/23.divide_candy_problem_test.cc
@@ -12,4 +12,17 @@ TEST(DIVIDE_CANDY_PROBLEM, DIVIDE_CANDY_PROBLEM_EXAMPLE_TEST) {
     ASSERT_EQ(DivideCandyProblem::min_candy_num1({2, 2, 4, 4, 2}), 7);
     ASSERT_EQ(DivideCandyProblem::min_candy_num1({1, 2, 3, 2, 3, 1}), 10);
     ASSERT_EQ(DivideCandyProblem::min_candy_num1({1, 5, 3, 2, 1, 1}), 12);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num1({5, 4, 3, 3, 2, 1}), 12);
+}
+
+TEST(DIVIDE_CANDY_PROBLEM, DIVIDE_CANDY_PROBLEM_ADV_EXAMPLE_TEST) {
+    ASSERT_EQ(DivideCandyProblem::min_candy_num2({2, 2, 1}), 5);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num2({1, 2, 2}), 5);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num2({1, 1, 1, 1}), 4);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num2({2, 2, 4, 4, 2}), 7);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num1({1, 2, 3, 2, 3, 1}), 10);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num2({5, 4, 3, 3, 2, 1}), 18);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num2({1, 2, 3, 3, 4, 5}), 18);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num2({1, 3, 3, 3, 2, 1}), 13);
+    ASSERT_EQ(DivideCandyProblem::min_candy_num2({1, 3, 3, 5, 5, 4, 3, 3, 2, 1, 3, 2, 2}), 32);
 }
